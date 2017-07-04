2bf183fbc7f94734682942ec0dbee14b3d72d855
raikkonen8097 <ehdghk7574@gmail.com>
2017-07-04 16:33:55 +0900

    test commit3

---
+++ openwsn-fw/openapps/cexample/cexample.c
@@ -0,0 +1,182 @@
+/**
+\brief An example CoAP application.
+*/
+
+#include "opendefs.h"
+#include "cexample.h"
+#include "opencoap.h"
+#include "opentimers.h"
+#include "openqueue.h"
+#include "packetfunctions.h"
+#include "openserial.h"
+#include "openrandom.h"
+#include "scheduler.h"
+
+#include "idmanager.h"
+#include "IEEE802154E.h"
+
+//=========================== defines =========================================
+
+/// inter-packet period (in ms)
+#define CEXAMPLEPERIOD  10000
+#define PAYLOADLEN      2
+
+const uint8_t cexample_path0[] = "ex";
+
+//=========================== variables =======================================
+
+cexample_vars_t cexample_vars;
+
+//=========================== prototypes ======================================
+
+owerror_t cexample_receive(OpenQueueEntry_t* msg,
+                    coap_header_iht*  coap_header,
+                    coap_option_iht*  coap_options);
+void    cexample_timer_cb(opentimer_id_t id);
+void    cexample_task_cb(void);
+void    cexample_sendDone(OpenQueueEntry_t* msg,
+                       owerror_t error);
+
+//=========================== public ==========================================
+
+void cexample_init() {
+   
+   // prepare the resource descriptor for the /ex path
+   cexample_vars.desc.path0len             = sizeof(cexample_path0)-1;
+   cexample_vars.desc.path0val             = (uint8_t*)(&cexample_path0);
+   cexample_vars.desc.path1len             = 0;
+   cexample_vars.desc.path1val             = NULL;
+   cexample_vars.desc.componentID          = COMPONENT_CEXAMPLE;
+   cexample_vars.desc.discoverable         = TRUE;
+   cexample_vars.desc.callbackRx           = &cexample_receive;
+   cexample_vars.desc.callbackSendDone     = &cexample_sendDone;
+   
+   
+   opencoap_register(&cexample_vars.desc);
+   cexample_vars.timerId    = opentimers_start(CEXAMPLEPERIOD,
+                                                TIMER_PERIODIC,TIME_MS,
+                                                cexample_timer_cb);
+}
+
+//=========================== private =========================================
+
+owerror_t cexample_receive(OpenQueueEntry_t* msg,
+                      coap_header_iht* coap_header,
+                      coap_option_iht* coap_options) {
+   return E_FAIL;
+}
+
+//timer fired, but we don't want to execute task in ISR mode
+//instead, push task to scheduler with COAP priority, and let scheduler take care of it
+void cexample_timer_cb(opentimer_id_t id){
+   scheduler_push_task(cexample_task_cb,TASKPRIO_COAP);
+}
+
+void cexample_task_cb() {
+   OpenQueueEntry_t*    pkt;
+   owerror_t            outcome;
+   uint8_t              i;
+   
+   uint16_t             x_int       = 0;
+   uint16_t             sum         = 0;
+   uint16_t             avg         = 0;
+   uint8_t              N_avg       = 10;
+   
+   // don't run if not synch
+   if (ieee154e_isSynch() == FALSE) return;
+   
+   // don't run on dagroot
+   if (idmanager_getIsDAGroot()) {
+      opentimers_stop(cexample_vars.timerId);
+      return;
+   }
+   
+   for (i = 0; i < N_avg; i++) {
+      sum += 1;
+   }
+   avg = 0xABCD;
+   //실제 패킷안에 들어가는 값
+
+   // create a CoAP RD packet
+   pkt = openqueue_getFreePacketBuffer(COMPONENT_CEXAMPLE);
+   if (pkt==NULL) {
+      openserial_printError(
+         COMPONENT_CEXAMPLE,
+         ERR_NO_FREE_PACKET_BUFFER,
+         (errorparameter_t)0,
+         (errorparameter_t)0
+      );
+      openqueue_freePacketBuffer(pkt);
+      return;
+   }
+   // take ownership over that packet
+   pkt->creator                   = COMPONENT_CEXAMPLE;
+   pkt->owner                     = COMPONENT_CEXAMPLE;
+   // CoAP payload
+   packetfunctions_reserveHeaderSize(pkt,PAYLOADLEN);
+   for (i=0;i<PAYLOADLEN;i++) {
+      pkt->payload[i]             = i;
+   }
+   
+   pkt->payload[0]                = (avg>>8)&0xff;
+   pkt->payload[1]                = (avg>>0)&0xff;
+   ////패킷 내용 담는 부분
+   packetfunctions_reserveHeaderSize(pkt,1);
+   pkt->payload[0] = COAP_PAYLOAD_MARKER;
+   
+   // content-type option
+   packetfunctions_reserveHeaderSize(pkt,2);
+   pkt->payload[0]                = (COAP_OPTION_NUM_CONTENTFORMAT - COAP_OPTION_NUM_URIPATH) << 4
+                                    | 1;
+   pkt->payload[1]                = COAP_MEDTYPE_APPOCTETSTREAM;
+   // location-path option
+   packetfunctions_reserveHeaderSize(pkt,sizeof(cexample_path0)-1);
+   memcpy(&pkt->payload[0],cexample_path0,sizeof(cexample_path0)-1);
+   packetfunctions_reserveHeaderSize(pkt,1);
+   pkt->payload[0]                = ((COAP_OPTION_NUM_URIPATH) << 4) | (sizeof(cexample_path0)-1);
+   
+   // metadata
+   pkt->l4_destination_port       = WKP_UDP_COAP;
+   pkt->l3_destinationAdd.type    = ADDR_128B;
+
+   pkt->l3_destinationAdd.addr_128b[0]  = 0xbb;
+   pkt->l3_destinationAdd.addr_128b[1]  = 0xbb;
+   pkt->l3_destinationAdd.addr_128b[2]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[3]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[4]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[5]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[6]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[7]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[8]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[9]  = 0x00;
+   pkt->l3_destinationAdd.addr_128b[10] = 0x00;
+   pkt->l3_destinationAdd.addr_128b[11] = 0x00;
+   pkt->l3_destinationAdd.addr_128b[12] = 0x00;
+   pkt->l3_destinationAdd.addr_128b[13] = 0x00;
+   pkt->l3_destinationAdd.addr_128b[14] = 0x00;
+   pkt->l3_destinationAdd.addr_128b[15] = 0x01;
+
+
+
+   //memcpy(&pkt->l3_destinationAdd.addr_128b[0],&ipAddr_ringmaster,16);
+   
+   // send
+   outcome = opencoap_send(
+      pkt,
+      COAP_TYPE_NON,
+      COAP_CODE_REQ_PUT,
+      1,
+      &cexample_vars.desc
+   );
+   
+   // avoid overflowing the queue if fails
+   if (outcome==E_FAIL) {
+      openqueue_freePacketBuffer(pkt);
+   }
+   
+   return;
+}
+
+void cexample_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
+   openqueue_freePacketBuffer(msg);
+}
