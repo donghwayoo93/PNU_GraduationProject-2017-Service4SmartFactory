2956cfc3432c866484adb6eef2f32db94443b24f
raikkonen8097 <ehdghk7574@gmail.com>
2017-07-08 18:48:06 +0900

    modification that rssi to meter

--- openwsn-sw/software/openvisualizer/openvisualizer/openType/typeRssi.py
+++ openwsn-sw/software/openvisualizer/openvisualizer/openType/typeRssi.py
@@ -9,6 +9,7 @@ log.setLevel(logging.ERROR)
 log.addHandler(logging.NullHandler())
 
 import openType
+import math
 
 class typeRssi(openType.openType):
     
@@ -25,7 +26,29 @@ class typeRssi(openType.openType):
     #======================== public ==========================================
     
     def update(self,rssi):
-        self.rssi = rssi
+        # modified by Yoo DongHwa
+        # 2017-07-08
+
+        # line below was the original statement
+        # self.rssi = rssi
+
+        # cc2420 base tx power value
+        TxPower = -31
+
+        # rssi to distance code
+        # referenced from
+        # https://www.quora.com/How-do-I-calculate-distance-in-meters-km-yards-from-rssi-values-in-dBm-of-BLE-in-android
+        # https://gist.github.com/eklimcz/446b56c0cb9cfe61d575
+
+        if(rssi == 0):
+            self.rssi = 0
+        else:
+            ratio = rssi * 1.0 / TxPower
+
+            if(ratio < 1.0):
+                self.rssi = math.pow(ratio, 10)
+            else:
+                self.rssi = math.pow(10, (TxPower - rssi) / (10 * 2))
     
     #======================== private =========================================
     
