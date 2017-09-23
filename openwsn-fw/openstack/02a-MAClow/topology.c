db2d53cd1eea6a0698e8d3f80de90f007b40c24f
nahyeoni <rozsa0409@naver.com>
2017-09-23 19:20:44 +0900

    topology modified

--- openwsn-fw/openstack/02a-MAClow/topology.c
+++ openwsn-fw/openstack/02a-MAClow/topology.c
@@ -79,7 +79,7 @@ bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
       case 0xbd:
          if (
                ieee802514_header->src.addr_64b[7]==0xec ||
-               //ieee802514_header->src.addr_64b[7]==0x02
+               ieee802514_header->src.addr_64b[7]==0x02
             ){
             returnVal=TRUE;
          }
@@ -93,7 +93,7 @@ bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
       case 0x3c:
          if (
                ieee802514_header->src.addr_64b[7]==0xec ||
-               //ieee802514_header->src.addr_64b[7]==0x02
+               ieee802514_header->src.addr_64b[7]==0x02
             ){
             returnVal=TRUE;
          }
@@ -107,7 +107,7 @@ bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
       case 0x79:
          if (
                ieee802514_header->src.addr_64b[7]==0xec ||
-               //ieee802514_header->src.addr_64b[7]==0x02
+               ieee802514_header->src.addr_64b[7]==0x02
             ){
             returnVal=TRUE;
          }
